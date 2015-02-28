2382dae63e5bf30bc0701e6ca4c62ed1bc3ecc3e
jronrun <furonglang@gmail.com>
2015-02-28 16:18:34 +0800

    benayn-ustyle upgrade to 1.0.1

--- benayn-ustyle-test/src/test/java/com/benayn/Me5Test.java
+++ benayn-ustyle-test/src/test/java/com/benayn/Me5Test.java
@@ -2,16 +2,16 @@ package com.benayn;
 
 import org.junit.Test;
 
+import com.benayn.ustyle.Objects2;
 import com.benayn.ustyle.Objects2.FacadeObject;
 
 public class Me5Test extends Me4Test {
 
     @Test
     public void testTmp() {
-        FacadeObject<User> fo = FacadeObject.wrap(User.class);
-        fo.populate4Test();
-        fo.info();
-        log.info(fo.toString());
+        FacadeObject<User> fo = Objects2.wrapObj(User.class);
+        fo.populate4Test().info();
+        
     }
     
 }
